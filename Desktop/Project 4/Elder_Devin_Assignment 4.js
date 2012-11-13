3d206bf7e6daeebbd4576ed95e2c3ae94dac81d5
Devin Lauren Elder <laurenelder@fullsail.edu>
2012-11-12 22:26:09 -0500

    Code Library Template.

---
+++ Desktop/Project 4/Elder_Devin_Assignment 4.js
@@ -0,0 +1,86 @@
+// Devin "Lauren" Elder
+// SDI Term 1211
+// Project 4 Code Library
+// "Date"
+
+// My Library
+
+var myLibrary = {
+
+// String Functions
+// String Phone Number Function
+
+	var phnNum = function(){
+	
+	};
+
+// String Email Function
+
+	var emlAdrs = function(){
+	
+	};
+
+// String Url Function
+
+	var myUrl = function(){
+	
+	};
+
+// String Titlecase Function
+
+	var titleCaseString = function(){
+	
+	};
+
+// String Separator Function
+
+	var separatorMod = function(){
+	
+	};
+
+// Number Functions
+// Number Decimal Config Function
+
+	var dcmlFrmt = function(){
+	
+	};
+
+// Number Fuzzy Match Function
+
+	var fzyMtchNum = function(){
+	
+	};
+
+// Number Difference Function
+
+	var differenceDate = function(){
+	
+	};
+
+// Number Actual Num Return Function
+
+	var numRtrn = function(){
+	
+	};
+
+// Array Functions
+// Array Smallest Value Function
+
+	var smallestValue = function(){
+	
+	};
+
+// Array Total Value Function
+
+	var totalValue = function(){
+	
+	};
+
+// Array Key Value Return Function
+
+	var keyValueRtrn = function(){
+	
+	};
+
+};
+
